39d997f1adb3d95dddc87f0fb51d15c7614438c7
Mikhael Dantas <mikhael.dcs@gmail.com>
2021-02-23 16:56:52 -0300

    day 2 completed

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,10 @@
+import 'reflect-metadata'
 import express from 'express'
-
+import './database'
+import { router } from './routes'
 const app = express()
 
-app.get('/', (req, res) => {
-    res.status(200).json({success: true})
-})
-
-app.post('/', (req, res) => {
-    res.status(200).json({success: "supertrue"})
-})
+app.use(express.json())
+app.use(router)
 
 app.listen(3333, () => {console.log('server is running')})
