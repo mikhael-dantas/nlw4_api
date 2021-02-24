1bce9028f5a8f9fd1413eff1d4939d31a8346a18
Mikhael Dantas <mikhael.dcs@gmail.com>
2021-02-24 18:37:36 -0300

    finishing tests setup

--- src/__tests__/First.test.ts
+++
@@ -1,5 +0,0 @@
-describe("First", () => {
-    it("must be possible sum 2 numbers", () => {
-        expect(2 + 2).toBe(4)
-    })
-})
---
+++ src/__tests__/Users.test.ts
@@ -0,0 +1,19 @@
+import request from 'supertest'
+import { app } from '../app'
+
+import createConnection from '../database'
+
+describe("Users", () => {
+    beforeAll(async () => {
+        const connection = await createConnection()
+        await connection.runMigrations()
+    })
+    it("should be able to create a new user", async () => {
+        const response = await request(app).post('/users').send({
+            email: 'user@example.com',
+            name: 'user example'
+        })
+
+        expect(response.status).toBe(201)
+    })
+})
