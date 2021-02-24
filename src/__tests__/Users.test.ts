4635ca2184eb63120e1b28374c0cfcf25a658400
Mikhael Dantas <mikhael.dcs@gmail.com>
2021-02-24 19:33:53 -0300

    day 3 finished

--- src/__tests__/Users.test.ts
+++ src/__tests__/Users.test.ts
@@ -1,27 +1,30 @@
-import request from 'supertest'
-import { app } from '../app'
+import request from 'supertest';
 
-import createConnection from '../database'
+import { app } from '../app';
 
-describe("Users", () => {
+import createConnection from '../database';
+
+describe('Users', () => {
     beforeAll(async () => {
-        const connection = await createConnection()
-        await connection.runMigrations()
-    })
-    it("should be able to create a new user", async () => {
-        const response = await request(app).post('/users').send({
-            email: 'user@example.com',
-            name: 'user example'
-        })
+        const connection = await createConnection();
+        await connection.runMigrations();
+    });
 
-        expect(response.status).toBe(201)
-    })
-    it("should be able to create a new user with an existing email", async () => {
+    it('Should be able to create a new user', async () => {
         const response = await request(app).post('/users').send({
-            email: 'user@example.com',
-            name: 'user example'
-        })
+        email: 'user@example.com',
+        name: 'User Example',
+        });
 
-        expect(response.status).toBe(400)
-    })
-})
+        expect(response.status).toBe(201);
+    });
+
+    it('should not be able to create a new user with an existing email', async () => {
+        const response = await request(app).post('/users').send({
+        email: 'user@example.com',
+        name: 'User Example',
+        });
+
+        expect(response.status).toBe(400);
+    });
+});
