988d34cdc65bd3c61faedfaa77085dda0b7a6079
Mikhael Dantas <mikhael.dcs@gmail.com>
2021-02-25 18:16:35 -0300

    day 4 completed

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,18 @@
 import { Router } from 'express'
 import { SurveysController } from './controllers/SurveysController'
 import { UsersController } from './controllers/UsersController'
+import { SendMailsController } from './controllers/SendMailsController'
 
 const router = Router()
 const usersController = new UsersController
 const surveysController = new SurveysController
+const sendMailsController = new SendMailsController
 
 router.post('/users', usersController.create)
 
 router.get('/surveys', surveysController.index)
 router.post('/surveys', surveysController.create)
+
+router.post('/sendMails', sendMailsController.execute)
 
 export {router}
